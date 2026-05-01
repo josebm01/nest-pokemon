435b21f0de676a223305cf76436110671223ac83
Jose BM <jbaxin@manuelsolis.com>
2026-05-01 16:54:50 -0600

    - A custom provider is created - Pagination is implemented to fetch the Pokemon

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // Elimina propiedades no definidas en el DTO
       forbidNonWhitelisted: true, // Lanza un error si se reciben propiedades no definidas
+      transform: true, // Transforma los datos al tipo definido en el DTO
+      transformOptions: {
+        enableImplicitConversion: true, // Permite la conversión implícita de tipos (e.g., string a number)
+      }
     })
   );
 
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
-
   // dependencias
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
   ){}  
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({}); //delete * from 
 
-    const { data } = await this.axios.get('https://pokeapi.co/api/v2/pokemon?limit=600');
+    const data = await this.http.get('https://pokeapi.co/api/v2/pokemon?limit=600') as { results: { name: string; url: string }[] };
+
+    //* FORMA 2
+    const pokemonToInsert: {name: string, no: number }[] = [];
+    
+    data.results.forEach(({ name, url }) => {
+
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      pokemonToInsert.push({name, no});
+    });
+
+    // una sola inserción
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed...';    
+
+
 
     //* FORMA 1
     // // insert multiples con promesas
@@ -38,22 +55,6 @@ export class SeedService {
 
     // await Promise.all(insertPromisesArray);
 
-
-    //* FORMA 2
-    const pokemonToInsert: {name: string, no: number }[] = [];
-    
-    data.results.forEach(({ name, url }) => {
-
-      const segments = url.split('/');
-      const no = +segments[segments.length - 2];
-
-      pokemonToInsert.push({name, no});
-    });
-
-    // una sola inserción
-    await this.pokemonModel.insertMany(pokemonToInsert);
-
-    return 'Seed executed...';
   }
 
 }
